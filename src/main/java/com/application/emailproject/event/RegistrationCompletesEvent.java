c77a7ee7a55fb7983321f2263b5aa5d5870322f1
Rakshith144-111 <rakshithpdrak@gmail.com>
2024-10-09 00:21:26 +0530

    Explanation added

--- src/main/java/com/application/emailproject/event/RegistrationCompletesEvent.java
+++ src/main/java/com/application/emailproject/event/RegistrationCompletesEvent.java
@@ -6,20 +6,21 @@ import com.application.emailproject.user.User;
 
 public class RegistrationCompletesEvent extends ApplicationEvent{
 	
+	//Constructor
 	public RegistrationCompletesEvent(User user, String applicationUrl) {
 		super(user);
 		this.user = user;
 		this.applicationUrl = applicationUrl;
 	}
 	
-	
+	//For which user we have to generate the URL
 	private User user;
 	//url which will be sent to user for the verification of the registration
 	private String applicationUrl;
 	
 	
 	
-	
+	//Getters
 	public User getUser() {
 		return user;
 	}

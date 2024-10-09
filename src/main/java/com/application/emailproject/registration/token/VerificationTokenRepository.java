23752badbda03fc03a16db831d13ad056e28861b
Rakshith144-111 <rakshithpdrak@gmail.com>
2024-10-09 23:29:13 +0530

    Changes configuration

--- src/main/java/com/application/emailproject/registration/token/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.emailproject.registration.token;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Long>{
-
-}

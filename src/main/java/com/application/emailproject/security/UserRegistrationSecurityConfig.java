984f5527260626ba559ddc1a470b69cc51a603f2
Rakshith144-111 <rakshithpdrak@gmail.com>
2024-10-09 22:51:40 +0530

    Succesful Registration

--- src/main/java/com/application/emailproject/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/application/emailproject/security/UserRegistrationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.application.emailproject.security;
+import org.springframework.security.config.Customizer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -15,5 +20,17 @@ public class UserRegistrationSecurityConfig {
 	{
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.cors(withDefaults()).csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/register/**")
+                        .permitAll())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/users/**")
+                        .hasAnyAuthority("USER", "ADMIN")).formLogin(withDefaults()).build();
+    }
+
 
 }
